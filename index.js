44cb706de2ecfad37be5c2ec688d87bdaedf8269
Aashish <112801519+Anshe3@users.noreply.github.com>
2022-09-09 00:57:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function clicked() {
+    var a = document.querySelector(".nav ul")
+    var b = document.querySelector(".burger")
+    a.classList.toggle("visi");
+    b.classList.toggle("toggle")
+}
+
+
+
+document.querySelector(".place1").addEventListener('click', () => {
+    alert("THANKS FOR SUBSCRIBING");
+})
